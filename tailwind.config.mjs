2b705e2bf570a9f7bf73fe8354b781e5c9ed4613
Gerard Rius <gerardriush@gmail.com>
2024-02-25 22:07:32 +0100

    v1

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+import animations from '@midudev/tailwind-animations'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary': '#ddd',
+				'secondary': '#e7bb41',				
+			}
+		},
+	},
+	plugins: [animations],
+}
+
